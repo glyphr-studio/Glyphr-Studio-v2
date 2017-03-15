f45620af41608aad9bc6e1b60b84249d6c9bac87
mattlag <mattlag@gmail.com>
2017-03-14 17:54:01 -0700

    Pan Tool, View Layers

--- src/plugins/EditCanvas/EditCanvas.jsx
+++ src/plugins/EditCanvas/EditCanvas.jsx
@@ -1,81 +1,49 @@
 import "./EditCanvas.scss";
 import EventUnit from "./../../lib/core/canvasEventStream/CanvasEventUnit";
+import EditCanvas from "./EditCanvasCore";
+import "./PenTool";
+import "./PanTool";
+import "./Layer";
 // import config from "./../../config/config";
 // import storage from "./../../lib/storage/Storage";
 
-import "./PenTool";
-import "./Layer";
-
 let ecee = new EventUnit("editCanvas", 3);
-
 
 export default React.createClass({
 
     componentDidMount() {
-
         let canvas = document.getElementById('editCanvas');
         paper.setup(canvas);
         settings.handleSize = 10;
         ecee.emit("ready", canvas);
 
+        let selectedGlyph = this.props.selectedGlyph;
+        EditCanvas.initCanvas(selectedGlyph);
+        this.resetCanvasView();
         // console.log("Selected glyph at EditCanvas: " + this.props.selectedGlyph);
         // console.log("GlyphrStudio global var: " + window.GlyphrStudio.currentProject);
-
-        let glyph, path;
-        if (this.props.selectedGlyph &&
-            window.GlyphrStudio &&
-            window.GlyphrStudio.currentProject &&
-            window.GlyphrStudio.currentProject.glyphs &&
-            window.GlyphrStudio.currentProject.glyphs[this.props.selectedGlyph]) {
-
-            glyph = window.GlyphrStudio.currentProject.glyphs[this.props.selectedGlyph];
-
-            let displayShapes = new paper.CompoundPath();
-            displayShapes.fillRule = 'nonzero';
-            displayShapes.fillColor = 'slategray';
-            displayShapes.strokeWidth = 1;
-            displayShapes.strokeColor = 'gray';
-
-
-            for (let p in glyph.children) {
-                if (glyph.children.hasOwnProperty(p)) {
-                    // console.log('adding child path');
-                    // console.log(JSON.stringify(glyph.children[p]));
-
-                    /*
-                        When paper.js exports obects through the exportJSON
-                        method, it returns an array of length=2:
-                        ["object name", {JSON properties}]
-                     */
-                    path = new paper.Path(glyph.children[p][1]);
-                    // console.log(path.exportJSON());
-                    displayShapes.addChild(path);
-                }
-            }
-
-            // console.log(displayShapes.exportJSON());
-            paper.view.draw();
-
-        } else {
-            console.warn('Edit Canvas - no selected glyph');
-        }
     },
 
-    clickOn() {
-
+    resetCanvasView(){
+        paper.view.center = new paper.Point(1400, 400);
+        paper.view.zoom = 1;
     },
+
+    clickOn() {},
 
     render() {
         return (
             <div className="centerFrame">
                 <div>
-                    <button onClick={ecee.emit.bind(ecee, "switchTool.penTool")}>Pen Tool</button>
-                    <button onClick={ecee.emit.bind(ecee, "clearCanvas")}>Clear Canvas</button>
+                    <button onClick={ecee.emit.bind(ecee, "switchTool.penTool")}>Pen Tool</button>&nbsp;
+                    <button onClick={ecee.emit.bind(ecee, "switchTool.panTool")}>Pan</button>&emsp;
+                    <button onClick={ecee.emit.bind(ecee, "clearCanvas")}>Clear Canvas</button>&nbsp;
+                    <button onClick={this.resetCanvasView}>Reset View</button>&nbsp;
                 </div>
                 <canvas id="editCanvas"
                         onClick={this.clickOn}
                         data-paper-resize={true}/>
             </div>
-        )
+        );
     }
 });
---
+++ src/plugins/EditCanvas/EditCanvasCore.js
@@ -0,0 +1,91 @@
+export default {
+
+    initCanvas(selectedGlyph) {
+        this.generateCanvasGrid(selectedGlyph);
+        let outlinesLayer = new paper.Layer();
+        outlinesLayer.name = 'outlines';
+        outlinesLayer.activate();
+
+        this.redrawCanvas(selectedGlyph);
+    },
+
+    redrawCanvas(selectedGlyph) {
+        console.log('redrawCanvas');
+
+        let glyph, path;
+        if (selectedGlyph && window.GlyphrStudio && window.GlyphrStudio.currentProject && window.GlyphrStudio.currentProject.glyphs && window.GlyphrStudio.currentProject.glyphs[selectedGlyph]) {
+
+            glyph = window.GlyphrStudio.currentProject.glyphs[selectedGlyph];
+
+            let displayShapes = new paper.CompoundPath();
+            displayShapes.fillRule = 'nonzero';
+            displayShapes.fillColor = 'slategray';
+            displayShapes.strokeWidth = 1;
+            displayShapes.strokeColor = 'gray';
+
+            for (let p in glyph.children) {
+                if (glyph.children.hasOwnProperty(p)) {
+                    // console.log('adding child path');
+                    // console.log(JSON.stringify(glyph.children[p]));
+
+                    /*
+                        When paper.js exports objects through the exportJSON
+                        method, it returns an array of length=2:
+                        ["object name", {JSON properties}]
+                        When initializing a new Paper object, only the
+                        JSON properties can be used (not the whole array)
+                    */
+                    path = new paper.Path(glyph.children[p][1]);
+                    // console.log(path.exportJSON());
+                    displayShapes.addChild(path);
+                }
+            }
+
+            // console.log(displayShapes.exportJSON());
+            paper.view.draw();
+
+        } else {
+            console.warn('Edit Canvas - no selected glyph');
+        }
+    },
+
+    generateCanvasGrid() {
+        console.log('generateCanvasGrid');
+
+        let gridLayer = new paper.Layer();
+        gridLayer.name = 'grid';
+        gridLayer.activate();
+
+        function line(pos, horizontal, color) {
+            let to, from, path;
+
+            if(horizontal){
+                from = new Point(pos, 3000);
+                to = new Point(pos, -3000);
+            } else {
+                from = new Point(3000, pos*-1);
+                to = new Point(-3000, pos*-1);
+            }
+
+            path = new Path.Line(from, to);
+            path.strokeColor = color;
+        }
+
+
+        // Grid lines
+        let chunk = 100;
+        for(let i=chunk; i<=chunk*10; i+=chunk){
+            line(i, true, '#eeeeee');
+            line(i, false, '#eeeeee');
+        }
+
+        let guidesLayer = new paper.Layer();
+        guidesLayer.name = 'guides';
+        guidesLayer.activate();
+
+
+        // Primary lines
+        line(0, true, '#ff9900');
+        line(0, false, '#993300');
+    }
+};
